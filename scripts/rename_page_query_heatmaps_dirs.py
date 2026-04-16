c6637611ad5c4ac234546f4f55a51b49ca3f3c5e
Hosein Erfan <hosein.hosein2011@gmail.com>
2026-04-16 11:59:19 -0400

    Shorten heatmap directory rename aliases

--- scripts/rename_page_query_heatmaps_dirs.py
+++ scripts/rename_page_query_heatmaps_dirs.py
@@ -7,15 +7,15 @@ from pathlib import Path
 
 
 RENAMES = {
-    "00c6daf76670c32dccf8effff5b552a7": "easy_ret4_success__who_sings_are_you_gonna_go_my_way__00c6daf76670c32dccf8effff5b552a7",
-    "e783cba0b3df36372d11823e378e5437": "hard_audit__lgbt_billionaires_bald_thick_glasses__e783cba0b3df36372d11823e378e5437",
-    "8a36aeac84a462745c5fa5ffacc45c1b": "hard_audit_mixed__ahl_pouncing_wild_cat_jersey__8a36aeac84a462745c5fa5ffacc45c1b",
-    "e1e6ed53f9ad11813845088f4cf2f6b1": "hard_audit__dzenis_beganovic_flower_logo__e1e6ed53f9ad11813845088f4cf2f6b1",
-    "18ecd2ac6c0ac69993b92dc4b30137e8": "hard_audit__ben_piazza_left_half_womans_face_poster__18ecd2ac6c0ac69993b92dc4b30137e8",
-    "d4d6487894e25da9ba73d0ffb39385b1": "imagelistq_ret4_correct_with_gold__bush_warbler_orange_stripe_eye__d4d6487894e25da9ba73d0ffb39385b1",
-    "c4bf958b6b268f7026de91e8d26da7e4": "imagelistq_ret4_correct_with_gold__trident_symbol_planet_neptune__c4bf958b6b268f7026de91e8d26da7e4",
-    "e9047078de74d8240ce689bdc504aea7": "imagelistq_ret4_correct_without_gold__trophy_golden_sphere_player__e9047078de74d8240ce689bdc504aea7",
-    "6105bed6e83b2189f0e2fd0fb1092822": "imagelistq_ret4_correct_without_gold__garth_jennings_two_police_officers__6105bed6e83b2189f0e2fd0fb1092822",
+    "00c6daf76670c32dccf8effff5b552a7": "easy_ret4__who_sings__00c6daf76670c32dccf8effff5b552a7",
+    "e783cba0b3df36372d11823e378e5437": "hard__lgbt_billionaires__e783cba0b3df36372d11823e378e5437",
+    "8a36aeac84a462745c5fa5ffacc45c1b": "hard_mixed__ahl_wild_cat__8a36aeac84a462745c5fa5ffacc45c1b",
+    "e1e6ed53f9ad11813845088f4cf2f6b1": "hard__flower_logo__e1e6ed53f9ad11813845088f4cf2f6b1",
+    "18ecd2ac6c0ac69993b92dc4b30137e8": "hard__ben_piazza_poster__18ecd2ac6c0ac69993b92dc4b30137e8",
+    "d4d6487894e25da9ba73d0ffb39385b1": "imgq_gold__warbler_eye__d4d6487894e25da9ba73d0ffb39385b1",
+    "c4bf958b6b268f7026de91e8d26da7e4": "imgq_gold__trident_neptune__c4bf958b6b268f7026de91e8d26da7e4",
+    "e9047078de74d8240ce689bdc504aea7": "imgq_nogold__golden_sphere__e9047078de74d8240ce689bdc504aea7",
+    "6105bed6e83b2189f0e2fd0fb1092822": "imgq_nogold__hot_fuzz__6105bed6e83b2189f0e2fd0fb1092822",
 }
 
 
